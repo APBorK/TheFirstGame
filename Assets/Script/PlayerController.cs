9f3abff7b207d3c3513bb002edeabf4cbc75c3ae
APBorK <1659732@gmail.com>
2021-02-09 02:19:42 +0300

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask whatCanBeClickedOn;
+    private NavMeshAgent myAgents;
+    private float speed = 8f;
+
+    void Start()
+    {
+        myAgents = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        //Движение
+        if (Input.GetMouseButton(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay,out hitInfo,100,whatCanBeClickedOn))
+            {
+                myAgents.SetDestination(hitInfo.point);
+            }
+
+            myAgents.speed = speed;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            myAgents.speed = speed * 2;
+        }
+        //...
+        
+        
+    }
+}
---
+++ Assets/Script/SpawnNPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNPC : MonoBehaviour
+{
+    public Transform Spawn;
+    public GameObject objToSpawn;
+
+    float maxX;
+    float minX;
+    float maxZ;
+    float minZ;
+    private int quantity = 10;
+    void Start()
+    {
+        maxX = Spawn.position.x + Spawn.localScale.x / 2;
+        minX = Spawn.position.x - Spawn.localScale.x / 2;
+
+        maxZ = Spawn.position.z + Spawn.localScale.z / 2;
+        minZ = Spawn.position.z - Spawn.localScale.z / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < quantity; i++)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(minX, maxX), Spawn.position.y, Random.Range(minZ, maxZ));
+            Instantiate(objToSpawn, spawnPos, Quaternion.identity);
+        }
+        
+    }
+}
