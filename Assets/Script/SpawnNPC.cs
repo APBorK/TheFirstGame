bb038b9b27ed292d63fc297b39146f8c3121a390
APBorK <1659732@gmail.com>
2021-02-10 11:15:07 +0300

    Update SpawnNPC.cs

--- Assets/Script/SpawnNPC.cs
+++ Assets/Script/SpawnNPC.cs
@@ -4,31 +4,19 @@ using UnityEngine;
 
 public class SpawnNPC : MonoBehaviour
 {
-    public Transform Spawn;
+    public Transform spawn;
     public GameObject objToSpawn;
 
-    float maxX;
-    float minX;
-    float maxZ;
-    float minZ;
-    private int quantity = 10;
+    
     void Start()
     {
-        maxX = Spawn.position.x + Spawn.localScale.x / 2;
-        minX = Spawn.position.x - Spawn.localScale.x / 2;
-
-        maxZ = Spawn.position.z + Spawn.localScale.z / 2;
-        minZ = Spawn.position.z - Spawn.localScale.z / 2;
+        Vector3 spawnPos = new Vector3(spawn.position.x,spawn.position.y,spawn.position.z);
+        Instantiate(objToSpawn, spawnPos, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < quantity; i++)
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(minX, maxX), Spawn.position.y, Random.Range(minZ, maxZ));
-            Instantiate(objToSpawn, spawnPos, Quaternion.identity);
-        }
-        
+
     }
 }
