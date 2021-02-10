fc9105d79347ec70e802c929bed5ae44bbfb7fb5
APBorK <1659732@gmail.com>
2021-02-10 13:08:38 +0300

    Update NPC.prefab

---
+++ Assets/Script/NPCMove.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour
+{
+    public LayerMask whatCanBeClickedOn;
+    private NavMeshAgent myAgents;
+    public Transform[] move;
+  
+
+    void Start()
+    {
+        myAgents = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        //Движение
+        
+       
+    }
+
+    private void OnTriggerEnter(Collider shit)
+    {
+        if (shit.gameObject.tag == "Spawn1")
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(move[0].position);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgents.SetDestination(hitInfo.point);
+            }
+            
+        }
+        
+        if (shit.gameObject.tag == "Spawn2")
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(move[1].position);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgents.SetDestination(hitInfo.point);
+            }
+            
+        }
+        
+        if (shit.gameObject.tag == "Spawn3")
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(move[2].position);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgents.SetDestination(hitInfo.point);
+            }
+            
+        }
+    }
+}
